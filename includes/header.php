1fe75136c48ec6e770bf3f06e836085fb9bae4f0
deez79 <code@derekisthecoolest.com>
2013-12-15 20:10:54 -0500

    Updating header to use sessions
    I have been trying to follow the script from 17.1, but
    I don't like the default sessions included, and I also
    would like to use cookies for this project.  I spent a lot
    of time setting up the login via cookies, and don't feel like
    scraping it.
    
    This commit is to save the work I did following the script.
    My next commit will likely be me hacking it to my needs/desires.

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,31 @@
 	<meta charset="UTF-8">
 	<title><?php echo $page_title; ?></title>
 	<link rel="stylesheet" href="/css/bootstrap.css" type="text\css" />
+	<?php //most of this php is from 17.1 (edited for my needs)
+
+	//start the session:
+	session_start();
+
+	//for testing purposes
+	$_SESSION['user_id'] = 1;
+	//for logging out
+	//$_SESSION = array();
+
+	//connect to the DB
+	require ('database/mysqli_connect.php');
+
+	//check for forum ID
+	// 	then store forum ID in the session:
+	if ( isset($_GET['fid']) && filter_var($_GET['fid'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+		$_SESSION['fid'] = $_GET['fid'];
+	} elseif ( !isset($_SESSION['fid'])) {
+		$_SESSION['fid'] = 1; // Default
+	}
+
+
+
+	?>
+
 </head>
 <body>
 	<div class="header">
@@ -13,17 +38,54 @@
 	<div class="nav">
 		<ul>
 			<li><a href="index.php">007 Archive</a></li>
-			<li><a href=""></li>
-			<li><a href=""></li>
+			<li><a href="forum.php">Forum Home</li>
+			<?php //Display links based on login status:
+			if (isset($_SESSION['user_id'])) {
+				//If this is the forum page, add a link for posting threads:
+				if (basename($_SERVER['PHP_SELF']) == 'forum.php') {
+					echo '<li><a href="post.php">New Thread</li>';
+				}
+				//add a log out link
+				echo '<li><a href="logout.php">Logout</li>' . "\n";
+			} else {
+				//Register and login
+				echo '<li><a href="login.php">Login</li>' . "\n";
+			}
+
+
+			//For Choosing a forum
+			echo '<form action="forum.php" method="get">
+			<li><select name="fid">
+				<option value="0">' . "Forums" . '</option>';
+
+				//Retrieve all Forum Names:
+				$q = "SELECT forum_id, forum_name FROM forum ORDER BY forum_id ASC";
+				$r = mysqli_query($dbc, $q);
+				if (mysqli_num_rows($r) > 0) {
+					while ($menu_row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+						echo "<option value=\"$menu_row[0]\">$menu_row[1]</option>\n";
+					} //end while statement
+				} //end if rows > 0
+				mysqli_free_result($r);
+			echo '</select></li>'; //end select statement
+
+			echo '<li><input name="submit" type="submit" value="submit" /></li>';
+			echo '</form>';
+
+			?>
+
+			<!-- //this is cookie based validation.
 			<li><a href=""></li>
 			<li><?php //Create a login/logout link:
+			/*
 				//checks to see if cookie is set, AND that you are not on the logout.php page
 				if ((isset($_COOKIE['user_id'])) && (basename($_SERVER['PHP_SELF']) !='logout.php')) {
 					echo '<a href="logout.php">Logout</a>';
 				} else{
 					echo '<a href="login.php">Login</a>';
 				}
-			?></li>
+			*/
+			?></li> -->
 			
 		</ul>
 	</div>	<!--End navigation-->
